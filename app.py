a5d6dc31123f399c7e60f2912183393dfb9a4c6a
Nam0vicH <wot12re@gmail.com>
2025-11-08 01:17:24 +0300

    initial commit on flask

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+import os
+
+# Получаем абсолютный путь к текущей директории
+template_dir = os.path.abspath(os.path.dirname(__file__))
+static_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'static'))
+
+app = Flask(__name__,
+            template_folder=os.path.join(template_dir, 'templates'),
+            static_folder=static_dir)
+
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+@app.route('/about-us')
+def about_us():
+    return render_template('about_us.html')
+
+@app.route('/order')
+def order():
+    return render_template('order.html')
+
+@app.route('/virtual-exhibition')
+def virtual_exhibition():
+    return render_template('virtual-exhi.html')
+
+@app.route('/poster')
+def poster():
+    return render_template('Poster.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
